2813539d6d990229fc5f809b4edd0c1c2488a939
Spirobel <spirobel@protonmail.com>
2025-05-06 20:33:32 +0800

    make route value be possibly not tied to specfic route

--- mininext/url.ts
+++ mininext/url.ts
@@ -16,10 +16,11 @@ export type HTTPMethod =
   | "OPTIONS";
 
 export type MiniNextRouteHandlerObject<T extends string> = {
-  [K in HTTPMethod]?: HtmlHandler<unknown, T>;
+  [K in HTTPMethod]?: HtmlHandler<unknown, T> | HtmlHandler;
 };
 export type MiniNextRouteValue<T extends string> =
   | HtmlHandler<unknown, T>
+  | HtmlHandler
   | MiniNextRouteHandlerObject<T>;
 export type BunRoutes<
   R extends { [K in keyof R]: RouterTypes.RouteValue<Extract<K, string>> }
